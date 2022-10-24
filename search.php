9bf960cee805b553119c38e3bb573e63a38d1d88
qwertious <80864866+qwertious@users.noreply.github.com>
2022-10-24 17:43:04 -0400

    First test

--- search.php
+++ search.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Search</title>
+    </head>
+    <body>
+        <h1>Search</h1>
+            <p>
+            <?php
+            $dir    = '/tmp';
+            $files1 = scandir($dir);
+            echo $files1;
+            ?>
+        </p>
+    </body>
+</html>
